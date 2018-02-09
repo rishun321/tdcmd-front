ecbb2cf3265f2fa92f67b6a16f9949fc2da839e1
yinxiangApp <y.lancelot422@gmail.com>
2018-02-09 14:03:57 +0900

    message complated
    message complated

--- src/components/common/messageModal.vue
+++ src/components/common/messageModal.vue
@@ -1,25 +1,21 @@
 <template>
-<div class="modal fade in" id="message-modal" tabindex="-1" role="dialog" aria-labelledby="messageModalLabel" aria-hidden="true" data-show="true" data-keyboard="false" data-backdrop="static">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h4 class="modal-title"><i :class="{'fa': true, 'fa-info-circle': type === 'info', 'fa-question-circle': type === 'select', 'fa-warning': type === 'warn'}" style="margin-right: 3px"></i>{{title}}</h4>
-      </div>
-      <div :class="{'modal-body': true, 'text-blue': (type === 'info') || (type === 'select'), 'text-danger': type === 'warn'}">
-        <div class="message-content">
-          {{message}}
-        </div>
-        <div class="message-detail text-muted bg-gray" v-if="detail !== ''">
-          {{detail}}
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button type="button" :class="{'btn': true, 'btn-primary': type === 'info', 'btn-danger': type === 'warn', 'pull-right': true}" data-dismiss="modal" v-show="type != 'select'">OK</button>
-        <button type="button" :class="{'btn': true, 'btn-primary': type === 'select', 'btn-danger': type === 'warn'}" data-dismiss="modal" v-show="type === 'select'" v-on:click="excuteYes">YES</button>
-        <button type="button" :class="{'btn': true, 'btn-primary': type === 'select', 'btn-danger': type === 'warn'}" data-dismiss="modal" v-show="type === 'select'" v-on:click="excuteNo">NO</button>
-      </div>
-    </div>
-  </div>
+<div class="fullscreen">
+  <md-dialog :md-active.sync="showDialog">
+    <md-dialog-title>{{title}}</md-dialog-title>
+    <md-tabs md-dynamic-height>
+      <md-tab md-label="Message">
+        <p>{{message}}</p>
+      </md-tab>
+      <md-tab md-label="Detail">
+        <p>{{detail}}</p>
+      </md-tab>
+    </md-tabs>
+    <md-dialog-actions>
+      <md-button class="md-primary md-raised" @click="showDialog = false" v-show="type != 'select'">OK</md-button>
+      <md-button class="md-primary md-raised" @click="showDialog = false" v-show="type == 'select'">YES</md-button>
+      <md-button class="md-accent md-raised" @click="showDialog = false" v-show="type == 'select'">NO</md-button>
+    </md-dialog-actions>
+  </md-dialog>
 </div>
 </template>
 
@@ -36,7 +32,8 @@ export default {
       message: '',
       detail: '',
       yes: null,
-      no: null
+      no: null,
+      showDialog: false
     }
   },
   mounted () {
@@ -49,7 +46,7 @@ export default {
       self.detail = error.detail || ''
       self.yes = yes
       self.no = no
-      $('#message-modal').modal('show')
+      self.showDialog = true
     })
   },
   beforeDestroy () {
@@ -71,49 +68,17 @@ export default {
 </script>
 
 <style scoped lang="scss">
-#message-modal {
-  z-index: 9999;
+
+.fullscreen {
+  width: 100%;
+  height: 100%;
+  display: -webkit-flex;
+  display: flex;
+  -webkit-justify-content: center;
+  justify-content: center;
+  text-align: center;
 }
-.modal-header {
-  position: relative;
-  padding: 12px 22px 10px;
-  background: #555299;
-  h4 {
-    color: #fff;
-    font-size: 22px;
-    font-weight: 700;
-  }
-  .close {
-    position: absolute;
-    top: 12px;
-    right: 17px;
-    margin: 0 auto;
-    color: #fff;
-    font-size: 30px;
-    text-shadow: none;
-    opacity: 1;
-    &:focus {
-      outline: none;
-    }
-  }
-}
-.modal-body {
-  min-height: 200px;
-  padding: 10px;
-}
-.message-content {
-  font-size: 16px;
-  padding: 20px;
-  min-height: 40px;
-}
-.message-detail {
-  font-size: 12px;
-  margin: 20px;
-  padding: 20px;
-  min-height: 140px;
-  word-break: break-all;
-}
-.btn {
-  width: 120px;
+.md-dialog {
+  max-width: 768px;
 }
 </style>
